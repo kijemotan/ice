aff02e8ccf4c71519abf7c84623178059f493ddd
the motan <kijemotan@gmail.com>
2024-06-24 03:42:56 +0200

    added visible arrow keys, jumping

--- main.lua
+++ main.lua
@@ -9,27 +9,48 @@ local y               -- -up +down
 local vy
 -- motan movement
 local speed = 10      -- left, right
-local gravity = 15    -- down
-local jumppower = 10  -- suddenly up
+local gravity = 40    -- down
+local jumppower = -15 -- suddenly up
+local grip = 0.2
+local midair = true
+local doublejump = false
 -- motan media
 local motan           -- sprite
 local motanh          -- height
 local motanw          -- width
+-- motan controls
+local left
 
 -- fun stuff
 local lastsec
 local sec
 
+-- control images
+local leftimg
+local downimg
+local upimg
+local rightimg
+local leftdraw
+local downdraw
+local updraw
+local rightdraw
+
 function love.load()  -- load assets
   love.graphics.setDefaultFilter('nearest','nearest')
+
   motan = love.graphics.newImage('graphics/motan.png')
+  leftimg = love.graphics.newImage('graphics/left.png')
+  downimg = love.graphics.newImage('graphics/down.png')
+  upimg = love.graphics.newImage('graphics/up.png')
+  rightimg = love.graphics.newImage('graphics/right.png')
+
   motanh = motan:getHeight()
   motanw = motan:getWidth()
   love.window.setMode(xres,yres,{})
   x = xres/2-motanw/2 -- center player
   y = yres/2-motanh/2
-  vx = 5
-  vy = -10
+  vx = 0
+  vy = 0
 
   sec = love.timer.getTime()
 end
@@ -37,18 +58,81 @@ end
 function love.update(dt)
   vy = vy+gravity*dt  -- apply gravity
 
-  lastsec = love.timer.getTime()
-  if lastsec-sec >= 1 then
-    sec = lastsec
-    vy = -10
+  if left then
+    vx = math.max(-speed,vx-speed*grip)
+  elseif right then
+    vx = math.min(speed,vx+speed*grip)
   end
 
   y = y+vy
-  y = y % yres
+  y = math.min(y,yres-motanh)
   x = x+vx
   x = x % xres
+
+  if y == yres-motanh then
+    midair = false
+    doublejump = true
+  else
+    midair = true
+  end
 end
 
 function love.draw()
   love.graphics.draw(motan,math.floor(x+0.5),math.floor(y+0.5))
+
+  if leftdraw then
+    love.graphics.draw(leftimg,0,16)
+  end
+  if downdraw then
+    love.graphics.draw(downimg,16,16)
+  end
+  if updraw then
+    love.graphics.draw(upimg,16,0)
+  end
+  if rightdraw then
+    love.graphics.draw(rightimg,32,16)
+  end
+end
+
+function love.keypressed(key, unused, isrepeat)
+  if key == "left" then
+    left = true
+    leftdraw = true
+  end
+  if key == "right" then
+    right = true
+    rightdraw = true
+  end
+  if key == "up" then
+    updraw = true
+    if not isrepeat then
+      if not midair then      -- 1st jump from the ground
+        vy = jumppower
+        doublejump = true
+      elseif doublejump then  -- 2nd jump in the air
+        vy = jumppower
+        doublejump = false
+      end
+    end
+  end
+  if key == "down" then
+    downdraw = true
+  end
+end
+
+function love.keyreleased(key)
+  if key == "left" then
+    left = false
+    leftdraw = false
+  end
+  if key == "right" then
+    right = false
+    rightdraw = false
+  end
+  if key == "up" then
+    updraw = false
+  end
+  if key == "down" then
+    downdraw = false
+  end
 end
